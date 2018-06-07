c57cf04ac60392ebffefa6fcc6fd4ccf78160392
arifur <arifcse7@gmail.com>
2018-06-08 00:00:25 +0600

    write file

--- app/views.py
+++ app/views.py
@@ -1,9 +1,12 @@
 import requests
+import csv
+
 from flask import (
     Blueprint, render_template
 )
 from flask import current_app, g
 from datetime import datetime, timedelta
+# from tabulate import tabulate
 
 bp = Blueprint('users', __name__, url_prefix='/users')
 
@@ -15,6 +18,7 @@ def users():
             'Auth-Token': current_app.auth_token,
         }
 
+    user_list = []
     res = requests.get(url="https://api.hubstaff.com/v1/users", headers=headers)
     if res.status_code == 200:
         users = res.json()['users']
@@ -25,13 +29,34 @@ def users():
                 project_list = project_res.json()['projects']
 
                 for project in project_list:
+
                     time_res = requests.get("https://api.hubstaff.com//v1/custom/by_date/my?start_date="+yesterday_date+"&end_date="+yesterday_date+"&projects=" + str(project['id']) + "users=" + str(user['id']), headers=headers)
                     time_json = time_res.json()
                     if len(time_json['organizations']) > 0:
                         if time_json['organizations'][0]['dates'][0]['users'][0]['projects'][0]['id'] == project['id'] and time_json['organizations'][0]['dates'][0]['users'][0]['id'] == user['id']:
                             project['duration'] = round((time_json['organizations'][0]['dates'][0]['duration']) / 60)
+
+                    if 'duration' in project and project['duration']:
+                        project_name = project['name'] + " (" + str(project['duration']) + " minutes)"
+                    else:
+                        project_name = project['name']
+                    user_list.append({
+                        user['name']: project_name
+                    })
+
                 user['projects'] = project_list
 
     else:
         users = []
+
+
+
+    keys = [user['name'] for user in users]
+
+    print (user_list)
+    with open('report.csv', 'w') as output_file:
+        dict_writer = csv.DictWriter(output_file, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(user_list)
+
     return render_template('users.html', users=users)
